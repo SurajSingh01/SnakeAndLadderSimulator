e3ab4ac489d933a00bb90e2495b72b107302c46f
SurajSingh01 <singhsurajd03@gmail.com>
2020-08-01 12:23:15 +0530

    Refactored : Removed the normal play condition

--- snakeLadderSimulator.sh
+++ snakeLadderSimulator.sh
@@ -6,10 +6,6 @@ echo "Welcome to snake Ladder Simulator"
 position=0
 INITIAL_POSITION=0
 END_POSITION=100
-#Assumed that size of ladder is 21
-ladder=21
-#Assumed that size of snake is 18
-snake=18
 
 echo "The intial position of the player will be $position"
 
@@ -17,27 +13,20 @@ function playingOption()
 {
 	diceRoll=$(( 1 + RANDOM % 6 ))
 	echo "The number obtained is $diceRoll"
-	option=$(( RANDOM % 4 ))
+	option=$(( RANDOM % 3 ))
 	case "$option" in
-		0)
+		1)
 			echo "No play , your postion = $position"
 			position=$position
 			;;
-		# Assumed that the ladder size is of 21 and the last ladder is at position 78
-		1) if [ $position -lt 70 ]
-			then
-				echo "Congrats You got a ladder ! move by $diceRoll forward "
-				position=$(( $position + $diceRoll +$ladder ))
-			fi
-			;;
 		2)
-			echo "Normal play ,move dice by $diceRoll forward"
+			echo "Congrats You got a ladder ! move by $diceRoll forward "
 			position=$(( $position + $diceRoll ))
 			;;
 
  		*)
 			echo "Ohh! you got a snake move by $diceRoll backword"
-			position=$(( $position + $diceRoll -$snake ))
+			position=$(( $position + $diceRoll ))
 			if [ $position -le $INITIAL_POSITION ]
 			then
 				position=0
